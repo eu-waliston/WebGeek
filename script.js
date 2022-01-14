3ffff2d8c56fd9b5b9ca8ff8f4697035151beb7a
Waliston Euripedes <goneisgone@hotmail.com>
2022-01-14 16:11:00 -0300

    Main Project

---
+++ script.js
@@ -0,0 +1,25 @@
+const navbarToglerBtn = document.querySelector('.navbar-toggler');
+const navbarCollapseDiv = document.querySelector('.navbar-collapse');
+
+navbarToglerBtn.addEventListener('click', () => {
+
+  navbarCollapseDiv.classList.toggle('show-navbar-collapse');
+
+  if (navbarCollapseDiv.classList.contains('show-navbar-collapse')) {
+    navbarToglerBtn.querySelector('img').src = "images/times-menu-icon.png";
+  } else {
+    navbarToglerBtn.querySelector('img').src = "images/ham-menu-icon.png";
+  }
+
+});
+
+// fixed navgation bar 
+const header = document.querySelector('.header');
+
+window.addEventListener('scroll', () => {
+  if(window.pageYOffset > 100) {
+    header.classList.add('fixed-navbar');
+  } else {
+    header.classList.remove('fixed-navbar');
+  }
+});
